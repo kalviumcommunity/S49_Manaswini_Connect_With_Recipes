d64f0020569c2091a8b79c37bc9163b7f4623c5c
manaswini <manaswini0623@gmail.com>
2024-12-20 12:14:51 +0530

    Removed unnecessary code

--- Server/src/connection.js
+++ Server/src/connection.js
@@ -2,13 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        // Replace YOUR_MONGODB_URI with your actual MongoDB connection string
-        const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/your_database_name";
+        const connectionString = process.env.MONGODB_URI;
         
-        const connection = await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connection = await mongoose.connect(connectionString);
         
         console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
